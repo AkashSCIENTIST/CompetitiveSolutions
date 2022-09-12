0d6c299907eb9ff4792ab23ee8f5cfc430a1b812
Akash S P <AkashSCIENTIST@users.noreply.github.com>
2022-09-12 07:10:13 +0530

    Updated code

--- Leetcode/1366. Rank Teams by Votes.py
+++ Leetcode/1366. Rank Teams by Votes.py
@@ -2,16 +2,14 @@ class Solution:
     def rankTeams(self, votes: List[str]) -> str:
         d = {}
         count = len(votes[0])
-        if count == 0:
-            return votes[0]
+        for i in votes[0]:
+            d[i] = [0]*count
         for i in votes:
-            for j,val in enumerate(i):
-                temp = d.get(val, {})
-                temp['total'] = temp.get('total', 0) + 1
-                temp[j+1] = temp.get(j+1, 0) + 1
-                d[val] = temp
-        stats = [(i,d[i]) for i in d]
-        fun = lambda x : [x[1]['total']] + [x[1].get(i, 0) for i in range(1,count+1)] + [-ord(x[0])]
+            for index,val in enumerate(i):
+                d[val][index] += 1
+        
+        stats = (tuple([sum(d[i])]+d[i]+[-ord(i)+65]) for i in d)
+        fun = lambda x : x
         stats = sorted(stats, key=fun, reverse=True)
         # print(stats)
-        return "".join((i[0] for i in stats))
+        return "".join(chr(65+ -i[-1]) for i in stats)
