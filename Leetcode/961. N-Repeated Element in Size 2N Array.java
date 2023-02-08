c1be58b0532a2d881493cb87679cc763f4b5b121
Akash S P <AkashSCIENTIST@users.noreply.github.com>
2023-02-08 22:08:20 +0530

    Create 961. N-Repeated Element in Size 2N Array.java

---
+++ Leetcode/961. N-Repeated Element in Size 2N Array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1]==nums[i]) return nums[i];
+        }
+        return -1;
+    }
+}
