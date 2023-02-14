6d08eaab8aad773041409816945f8550ee6f75d5
Akash S P <AkashSCIENTIST@users.noreply.github.com>
2023-02-14 17:58:22 +0530

    Create 1446. Consecutive Characters.java

---
+++ Leetcode/1446. Consecutive Characters.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxPower(String s) {
+        char[] chars = s.toCharArray();
+        int count = 1;
+        int maxCount = count;
+        for(int i=1;i<chars.length;i++){
+            // System.out.println(chars[i] + " " + count);
+            if(chars[i] == chars[i-1]) count++;
+            else{
+                maxCount = Math.max(count, maxCount);
+                count = 1;
+            }
+        }
+        maxCount = Math.max(count, maxCount);
+        return maxCount;
+    }
+}
