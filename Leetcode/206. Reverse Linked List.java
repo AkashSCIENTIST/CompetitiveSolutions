9bbae7abaea1b1adedcbffa048b1cc9e0ce97d0c
Akash S P <AkashSCIENTIST@users.noreply.github.com>
2024-03-19 08:20:57 +0530

    Create 206. Reverse Linked List.java

---
+++ Leetcode/206. Reverse Linked List.java
@@ -0,0 +1,15 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode cur = head;
+        ListNode next = null;
+
+        while(cur != null){
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+}
