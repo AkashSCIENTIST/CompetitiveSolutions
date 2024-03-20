b1b4c5bc76545cadd60e805599c49e2c068e3667
Akash S P <AkashSCIENTIST@users.noreply.github.com>
2024-03-20 16:54:32 +0530

    Update 746. Min Cost Climbing Stairs.java

--- Leetcode/746. Min Cost Climbing Stairs.java
+++ Leetcode/746. Min Cost Climbing Stairs.java
@@ -14,3 +14,27 @@ class Solution {
         return Integer.min(total[0], total[1]);
     }
 }
+
+/*
+
+import java.math.*;
+
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int total[] = new int[n+1];
+        total[0] = 0;
+        total[1] = 0;
+        
+        // System.out.println(Arrays.toString(total));
+        for(int i=2;i<n+1;i++){
+            total[i] = Integer.min(total[i-1]+cost[i-1], total[i-2]+cost[i-2]);
+            // System.out.println(Arrays.toString(total));
+        }
+        // System.out.println(Arrays.toString(total));
+
+        return total[n];
+    }
+}
+
+*/
