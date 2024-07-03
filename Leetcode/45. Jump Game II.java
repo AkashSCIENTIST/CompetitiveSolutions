b4717b11023d21f9d05ca5a082e22733f7e0841f
Akash <spakash182@gmail.com>
2024-07-03 08:32:33 +0530

    Reorganized Leetcode

--- Leetcode/45. Jump Game II.java
+++ Leetcode/45. Jump Game II.java
@@ -1,17 +1,19 @@
 class Solution {
     public int jump(int[] nums) {
-        int ans = 0;
         int n = nums.length;
-        int currEnd = 0;
-        int currFarthest = 0;
+        int jumps = 0;
+        int l=0, r=0, fur=0;
         
-        for(int i = 0;i<n-1;i++){
-            currFarthest = Math.max(currFarthest, i+ nums[i]);
-            if(i==currEnd){
-                ans++;
-                currEnd = currFarthest;
+        while(r < n-1){
+            fur = 0;
+            for(int i=l;i<=r;i++){
+                fur = Math.max(fur, i+nums[i]);
             }
+            l = r+1;
+            r = fur;
+            jumps++;
         }
-        return ans;
+
+        return jumps;
     }
 }
