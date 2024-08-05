c2133144a8a8206d3ca2468018d08e98dcbe1c7e
Akash S P <AkashSCIENTIST@users.noreply.github.com>
2024-08-05 08:20:21 +0530

    Create 8. String to Integer (atoi).java

---
+++ Leetcode/1-999/8. String to Integer (atoi).java
@@ -0,0 +1,20 @@
+class Solution {
+    public int myAtoi(String s) {
+        s = s.strip();
+        if (s.isEmpty()) return 0;
+
+        int sign = s.charAt(0) == '-' ? -1 : 1;
+        if (s.charAt(0) == '+' || s.charAt(0) == '-') s = s.substring(1);
+
+        long num = 0;
+
+        for (char c : s.toCharArray()) {
+            if (!Character.isDigit(c)) break;
+            num = num * 10 + (c - '0');
+            if (sign * num <= Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            if (sign * num >= Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        }
+
+        return sign * (int) num;
+    }
+}
