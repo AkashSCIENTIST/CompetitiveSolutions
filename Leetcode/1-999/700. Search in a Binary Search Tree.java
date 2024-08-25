64be0d69660ebd4dee243a0bbae4424c0910240d
Akash S P <AkashSCIENTIST@users.noreply.github.com>
2024-08-25 09:47:30 +0530

    Create 700. Search in a Binary Search Tree.java

---
+++ Leetcode/1-999/700. Search in a Binary Search Tree.java
@@ -0,0 +1,8 @@
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null) return root;
+        if(root.val == val) return root;
+        if(root.val < val) return searchBST(root.right, val);
+        else return searchBST(root.left, val);
+    }
+}
