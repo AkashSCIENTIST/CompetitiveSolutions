0c8d4d067d711f39e9fc6623ba3300a1e2528a8f
Akash S P <AkashSCIENTIST@users.noreply.github.com>
2024-09-08 15:57:05 +0530

    Create 191. Number of 1 Bits (2).java

---
+++ Leetcode/1-999/191. Number of 1 Bits (2).java
@@ -0,0 +1,10 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int res = 0;
+        while(n>0) {
+            res += n&1;
+            n >>=1;
+        }
+        return res;
+    }
+}
