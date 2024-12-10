45c2a664d4dbe2f70bffbcb55f6f09b7d74277df
Akash S P <AkashSCIENTIST@users.noreply.github.com>
2024-12-10 16:46:05 +0530

    Create 383. Ransom Note.java

---
+++ Leetcode/1-999/383. Ransom Note.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int count[] = new int[26];
+
+        for(char c : magazine.toCharArray()) count[c-'a']++;
+        for(char c : ransomNote.toCharArray()) {
+            if(count[c-'a'] == 0) return false;
+            count[c-'a']--;
+        }
+
+        return true;
+    }
+}
