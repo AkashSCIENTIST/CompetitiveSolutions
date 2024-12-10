5f2cd1b3e6a5484486620b1e9ea36a069da6297b
Akash S P <AkashSCIENTIST@users.noreply.github.com>
2024-12-10 21:41:51 +0530

    Update 205. Isomorphic Strings.java

--- Leetcode/1-999/205. Isomorphic Strings.java
+++ Leetcode/1-999/205. Isomorphic Strings.java
@@ -3,12 +3,17 @@ class Solution {
         int[] smap = new int[256];
         int[] tmap = new int[256];
         int n = s.length();
-        if(n != t.length()) return false;
+        
+        if (n != t.length()) return false;
 
-        for(int i=0;i<n;i++){
-            if(smap[s.charAt(i)] != tmap[t.charAt(i)]) return false;
-            smap[s.charAt(i)] += 1;
-            tmap[t.charAt(i)] += 1;
+        for (int i = 0; i < n; i++) {
+            char sc = s.charAt(i);
+            char tc = t.charAt(i);
+
+            if (smap[sc] != tmap[tc]) return false;
+
+            smap[sc] = i + 1;
+            tmap[tc] = i + 1;
         }
 
         return true;
