fd24d901ddd5fe9316b4c5d8c4b2f86602cff4b0
Akash S P <AkashSCIENTIST@users.noreply.github.com>
2024-12-21 10:22:15 +0530

    Create 18. 4Sum.java

---
+++ Leetcode/1-999/18. 4Sum.java
@@ -0,0 +1,47 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            for (int j = i + 1; j < n; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+
+                int k = j + 1;
+                int l = n - 1;
+
+                while (k < l) {
+                    
+                    long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if (sum == target) {
+                        List<Integer> list1 = new ArrayList<>();
+                        list1.add(nums[i]);
+                        list1.add(nums[j]);
+                        list1.add(nums[k]);
+                        list1.add(nums[l]);
+                        list.add(list1);
+                        k++;
+                        l--;
+                        while (k < l && nums[k] == nums[k - 1])
+                            k++;
+                        while (k < l && nums[l] == nums[l + 1])
+                            l--;
+                    } else if (sum < target)
+                        k++;
+                    else
+                        l--;
+                }
+            }
+        }
+
+        return list;
+    }
+}
